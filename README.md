df97af19d4190fa550e476df80efa196bc139cb4
Maziyar Amini <mzyramini@gmail.com>
2024-02-10 03:04:35 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ReGram
+an Instagram interface clone with some changes that I liked to apply.
+a hobby project made with React.
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Menu from './components/Menu'
+
+function App() {
+
+  return (
+    <>
+        <Menu />
+    </>
+  )
+}
+
+export default App
